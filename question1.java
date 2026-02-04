53588357ea6be8c3382fde2a730ee7a79941f698
Chelsea Rice <officialchelsearice@gmail.com>
2026-02-03 23:16:21 -0500

    Adding solved question1.java & start of question2.java

---
+++ question1.java
@@ -0,0 +1,10 @@
+/*
+*      Question - Write a Hello World Program in Java
+*       By Chelsea Rice
+*/
+
+public class question1{
+    public static void main (String[] args){
+        System.out.print("Hello World");
+    }
+}
---
+++ question2.java
@@ -0,0 +1,10 @@
+/*
+*      Question - Write a program in Java to add 2 numbers
+*       By Chelsea Rice
+*/
+
+public class question2 {
+    public static void main (String[] args){
+        
+    }
+}
