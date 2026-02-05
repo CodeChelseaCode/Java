9bf0012db022f838eb01f803c57496cadd7eaad2
Chelsea Rice <officialchelsearice@gmail.com>
2026-02-05 18:49:33 -0500

    questions added
    answered q1 & part of q2

--- question2.java
+++ question2.java
@@ -5,6 +5,19 @@
 
 public class question2 {
     public static void main (String[] args){
+        addNumbers();
+    }
+
+
+    public static void addNumbers(){
+        int a = 5;
+        int b = 6;
+
+        System.out.println("5 + 6 = " + (a + b));
+    }
+
+    public static void enterAndaddNumbers(){
         
     }
+
 }
