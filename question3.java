cbdaff831c80861106c2811cfc80f143318a19a5
Chelsea Rice <officialchelsearice@gmail.com>
2026-02-06 20:51:19 -0500

    started question4.java and finished question3.java
    - completed question3.java
    - started structure of question4.java

--- question3.java
+++ question3.java
@@ -2,13 +2,43 @@
     Question - Write a Program to Swap Two Numbers
         Solved By Chelsea Rice
 */
+import java.util.Scanner;
+
 public class question3{
     public static void main(String[] args){
-
+        enterAndSwapNums();
     }
 
     public static void swapNums(){
+        int x = 2;
+        int z = 3;
         
+        System.out.println("This is before switching. X = " + x + " Z = " + z);
+        int temp = x;
+        x = z;
+        z = temp;
 
+        System.out.println("This is after switching. X = " + x + " Z = " + z);
+
+    }
+
+    public static void enterAndSwapNums(){
+        Scanner myScan = new Scanner(System.in);
+
+        System.out.print("Enter a X value: ");
+        int x = myScan.nextInt();
+
+        System.out.print("Enter a Z value: ");
+        int z = myScan.nextInt();
+
+        System.out.println("This is before switching. X = " + x + " Z = " + z);
+
+        int temp = x;
+        x = z;
+        z = temp;
+
+        System.out.println("This is after switching. X = " + x + " Z = " + z);
+
+        myScan.close();
     }
 }
---
+++ question4.java
@@ -0,0 +1,17 @@
+/*
+    Question - Write a Java Program to convert Integer numbers and Binary numbers.
+        Solved By Chelsea Rice
+*/
+public class question4 {
+    public static void main(String[] args){
+
+    }
+
+    public static void intToBinary(){
+        
+    }
+
+    public static void enterIntToBinary(){
+
+    }
+}
